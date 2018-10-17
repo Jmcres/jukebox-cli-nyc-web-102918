7d0d26a5c065106dba1b9daee1126ebc0af048ad
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 19:21:30 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def list(songs)
     if user_request.to_i == i
     puts "playing #{songs[i-1]}"
   elsif user_request == song 
-  puts "Playing #{song[i]}"
+  puts "Playing #{song}"
 else puts "Invalid input, please try again"
   end 
 end 
