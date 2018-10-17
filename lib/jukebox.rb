6c8b95a402a02664c57bd0f5c02fc55b7de0ee80
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 19:26:48 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,17 +25,10 @@ def list(songs)
   
   
   def play(songs)
-   puts "Please enter a song name or number:"
-  user_request = gets.chomp
-  songs.each_with_index do |song, i|
-    if user_request.to_i == i
-    puts "Playing #{songs[i-1]}"
-  elsif user_request == song 
-  puts "playing #{song[i]}"
-else puts "Invalid input, please try again"
-  end 
-end 
-end 
-
+     puts "Please enter a song name or number"
+  input = gets.chomp
+  songs.each_with_index do |song, idx|
+    if input.to_i == (idx+1)
+      puts 
 
   
